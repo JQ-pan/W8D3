5d7b21dbb10250dbad1b9cf4061be213fe1e9d4f
Mlkz <maho.mamulashvili@gmail.com>
2023-01-04 15:07:20 -0500

    add twoSum() uniq()

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -1,10 +1,26 @@
-Array.prototype.uniq = () => {
+Array.prototype.uniq = function() {
     let output = [];
-    let originalArray = this;
-    for (let i = 0; i < originalArray.length; i++) {
-        if (!output.includes(originalArray[i])) {
-            output.push(originalArray[i]);
+
+    for (let i = 0; i < this.length; i++) {
+        if (!output.includes(this[i])) {
+            output.push(this[i]);
         }
     }
-    return output
+
+    return output;
+}
+
+
+Array.prototype.twoSum = function() {
+    let output = [];
+
+    for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+            if (this[i] + this[j] === 0) {
+                output.push([i, j]);
+            }
+        }
+    }
+
+    return output;
 }
